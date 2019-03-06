abbac52b241ec95c939288e954a30ae7710b5089
sohel <sohel@192.168.0.5>
2019-03-06 17:40:03 -0500

    hi

--- Sohel/src/main/java/com/tt/Sohel/Apple.java
+++
@@ -1,9 +0,0 @@
-package com.tt.Sohel;
-
-public class Apple {
-	public void simpleMessage(String name) {
-		System.out.println("Hello"+name);
-	
-	}//end of simpleMessage
-
-}//main class
--- Sohel/src/main/java/com/tt/Sohel/NewClass.java
+++
@@ -1,8 +0,0 @@
-package com.tt.Sohel;
-
-public class NewClass {
-   public void simplemessage(String name) {
-	   System.out.println("hello"+name);
-	
-   }
-}//end of new class
